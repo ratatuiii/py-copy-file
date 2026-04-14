89ed274ec73105df8886489e70d32f8485c486dc
naomi <orest.chupa@gmail.com>
2026-04-14 16:43:33 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(argument: str) -> None:
+    if argument in ["", " "] or argument.count(" ") != 2:
+        return None
+
+    command, file1, file2 = argument.split(" ")
+
+    if command != "cp" or file1 == file2:
+        return None
+
+    try:
+        with open(file1, "r") as file_in, open(file2, "w") as file_out:
+            for line in file_in.readlines():
+                file_out.write(line)
+    except FileNotFoundError:
+        return None
