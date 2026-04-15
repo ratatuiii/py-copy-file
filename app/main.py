681c7d980bd55d6a9d5615dc4de03cce31725818
naomi <orest.chupa@gmail.com>
2026-04-15 09:16:55 +0300

    Naming improvement

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
-def copy_file(argument: str) -> None:
-    if argument in ["", " "] or argument.count(" ") != 2:
+def copy_file(command: str) -> None:
+    if command in ["", " "] or command.count(" ") != 2:
         return None
 
-    command, file1, file2 = argument.split(" ")
+    command, source_file_name, destination_file_name = command.split(" ")
 
-    if command != "cp" or file1 == file2:
+    if command != "cp" or source_file_name == destination_file_name:
         return None
 
     try:
-        with open(file1, "r") as file_in, open(file2, "w") as file_out:
+        with (
+            open(source_file_name, "r") as file_in,
+            open(destination_file_name, "w") as file_out,
+        ):
             for line in file_in.readlines():
                 file_out.write(line)
     except FileNotFoundError:
